4166fb663171e635f81a1cbd2f41dd3593ca2cff
MapleWarrior <michaelharborg@gmail.com>
2023-08-08 15:57:36 +0200

    Update result_helper_function.py

--- result_helper_function.py
+++ result_helper_function.py
@@ -15,8 +15,8 @@ def result_helper_function(params):
     p = 6
     n_iter = 2000
     reps = 10
-    AA_types = ['RBOAA_hotstart', 'RBOAA_alternating', 'RBOAA_betareg', 'RBOAA_Alternating_Betareg',
-                'OAA_hotstart', 'OAA_alternating', 'OAA_betareg', 'OAA_Alternating_Betareg', 'CAA_']
+    AA_types = ['RBOAA_hotstart', 'RBOAA_alternating', 'RBOAA_betareg', 'RBOAA_alternating_betareg',
+                'OAA_hotstart', 'OAA_alternating', 'OAA_betareg', 'OAA_alternating_betareg', 'CAA_']
     #AA_types = ["RBOAA", "OAA"]
 
     s = params[0]
@@ -51,57 +51,52 @@ def result_helper_function(params):
 
     for analysis_type in AA_types:
         AA_type = analysis_type.split('_')[0]
+
         if AA_type == 'CAA':
             lr = 0.1
         else:
             lr = 0.01
-        
+
+        beta_regulators = False
+        alternating = False
+
+        if alternating in analysis_type:
+            alternating = True
+        if beta_regulators in analysis_type:
+            beta_regulators = True
+
         for analysis_arch in analysis_archs:
             for rep in range(reps):
-
+                
                 AA_types_list.append(analysis_type)
                 analysis_archs_list.append(analysis_arch)
                 reps_list.append(rep)
 
-                if 'betareg' in analysis_type:
-                    AAM.analyse(model_type = AA_type, lr=lr, with_synthetic_data = True, mute=True, K=analysis_arch, n_iter = n_iter, with_hot_start=True, p=p, beta_regulators=True)
-                
-                elif 'alternating' in analysis_type:
-                    AAM.analyse(model_type = AA_type, lr=lr, with_synthetic_data = True, mute=True, K=analysis_arch, n_iter = n_iter, with_hot_start=True, p=p, alternating = True)
-                
-                elif 'hotstart' in analysis_type:
-                    AAM.analyse(model_type = AA_type, lr=lr, with_synthetic_data = True, mute=True, K=analysis_arch, n_iter = n_iter, with_hot_start=True, p=p)
-                
-                elif 'Alternating_Betareg' in analysis_type:
-                    AAM.analyse(model_type = AA_type, lr=lr, with_synthetic_data = True, mute=True, K=analysis_arch, n_iter = n_iter, with_hot_start=True, p=p, alternating=True, beta_regulators=True)
-                
-                elif 'CAA' in analysis_type:
-                    AAM.analyse(model_type = AA_type, lr=lr, with_synthetic_data = True, mute=True, K=analysis_arch, n_iter = n_iter, with_hot_start=True, p=p)
+                AAM.analyse(model_type=AA_type, lr=lr, with_synthetic_data = True, mute=True, K=analysis_arch, n_iter = n_iter, with_hot_start=True, p=p, beta_regulators=beta_regulators, alternating=alternating)
 
-                
                 analysis_A = AAM._synthetic_results[AA_type][0].A
                 analysis_Z = AAM._synthetic_results[AA_type][0].Z
 
-                #if AA_type == "CAA":
-                #    loss = AAM._synthetic_results[AA_type][0].RSS[-1]
-                #    BDM_list.append("NaN")
-                #    sigma_est_list.append("NaN")
-                #    losses_list.append(loss)
-                #    NMIs_list.append(NMI(analysis_A,syn_A))
-                #    MCCs_list.append(MCC(analysis_Z,syn_Z))
+                if AA_type == "CAA":
+                    loss = AAM._synthetic_results[AA_type][0].RSS[-1]
+                    BDM_list.append("NaN")
+                    sigma_est_list.append("NaN")
+                    losses_list.append(loss)
+                    NMIs_list.append(NMI(analysis_A,syn_A))
+                    MCCs_list.append(MCC(analysis_Z,syn_Z))
                 
-                #else:
-                loss = AAM._synthetic_results[AA_type][0].loss[-1]
-                analysis_betas = AAM._synthetic_results[AA_type][0].b
-                #print(AA_type)
-                #print(analysis_betas.shape)
-                BDM_list.append(BDM(syn_betas,analysis_betas,AA_type))
-                sigma_est_list.append(np.mean(AAM._synthetic_results[AA_type][0].sigma))
-                losses_list.append(loss)
-                NMIs_list.append(NMI(analysis_A,syn_A.T))
-                MCCs_list.append(MCC(analysis_Z,syn_Z.T))
-                #print(BDM(syn_betas,analysis_betas,AA_type))
-                #print(NMI(analysis_A,syn_A.T))
+                else:
+                    loss = AAM._synthetic_results[AA_type][0].loss[-1]
+                    analysis_betas = AAM._synthetic_results[AA_type][0].b
+                    #print(AA_type)
+                    #print(analysis_betas.shape)
+                    BDM_list.append(BDM(syn_betas,analysis_betas,AA_type))
+                    sigma_est_list.append(np.mean(AAM._synthetic_results[AA_type][0].sigma))
+                    losses_list.append(loss)
+                    NMIs_list.append(NMI(analysis_A,syn_A.T))
+                    MCCs_list.append(MCC(analysis_Z,syn_Z.T))
+                    #print(BDM(syn_betas,analysis_betas,AA_type))
+                    #print(NMI(analysis_A,syn_A.T))
 
 
     dataframe = pd.DataFrame.from_dict({
