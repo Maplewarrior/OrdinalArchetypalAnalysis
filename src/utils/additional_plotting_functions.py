4eb978bb5c967ca04b889a9e654846d2ec875906
Wedenborg <emiliewedenborg@gmail.com>
2024-04-25 11:26:21 +0200

    New plot func

--- src/utils/additional_plotting_functions.py
+++ src/utils/additional_plotting_functions.py
@@ -6,7 +6,11 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib
+import seaborn as sns
 
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
 
 def loss_archetype_plot(results_path: str = 'synthetic_results/1000_complex_results.json'):
     """
@@ -16,10 +20,11 @@ def loss_archetype_plot(results_path: str = 'synthetic_results/1000_complex_resu
         result = json.load(f)
         df_res = pd.DataFrame(result)
 
-    
+    df_res.method = df_res.method.replace({'OAA': 'OAA', 'RBOAA': 'RBOAA', 'AA': 'AA', 'CAA': 'AA'})    
     methods = df_res['method'].unique()
-    methods_colors = dict(zip(methods.tolist(), ['r', 'g', 'b', 'y']))
-    plt.figure(figsize=(6,4))
+
+    methods_colors = dict(zip(methods.tolist(), ["#EF476F", "#FFD166", "#06D6A0", "#073B4C"]))
+    plt.figure(figsize=(15,5))
 
     def add_curve(analysis_archetypes, losses, is_min: bool, method: str):
         if is_min:
@@ -45,12 +50,41 @@ def loss_archetype_plot(results_path: str = 'synthetic_results/1000_complex_resu
 
         for rep in range(losses.shape[1]):
             add_curve(analysis_archetypes, losses[:, rep], is_min=False, method=method)
-        
-        plt.xlabel('n.o. archetypes')
-        plt.ylabel('loss')
-        plt.legend()
-        plt.title(f'Final loss as a function of number of archetypes.')
+
+    
+        plt.xticks(fontsize=15)
+        plt.yticks(fontsize=15)    
+        plt.xlabel('Number of archetypes', fontsize=20)
+        plt.ylabel('Loss', fontsize=20)
+        plt.legend(fontsize=20)
     plt.show()
-    # print(losses)
 
 # loss_archetype_plot(df_res_20)
+
+def NMI_archetypes(results_path: str = 'synthetic_results/1000_complex_results.json'):
+    """
+    A plot over the final loss obtained as a function of the number of archetypes
+    """
+    with open(f'{results_path}', 'r') as f:
+        result = json.load(f)
+        df_res = pd.DataFrame(result)
+
+    df_res.method = df_res.method.replace({'OAA': 'OAA', 'RBOAA': 'RBOAA', 'AA': 'AA', 'CAA': 'AA'})
+
+    
+    methods = df_res['method'].unique()
+    methods_colors = dict(zip(methods.tolist(), ["#EF476F", "#FFD166", "#06D6A0", "#073B4C"]))
+    fig, ax = plt.subplots(1,1,figsize = (15,5))
+
+    ax = sns.boxplot(x='n_archetypes', y="NMI", hue="method", showmeans=True, data=df_res,palette=methods_colors,meanprops={"marker": "s", "markerfacecolor": "white", "markeredgecolor": "black"})
+    ax.xaxis.grid(True, which='major')
+    [ax.axvline(x+.5,color='k') for x in ax.get_xticks()]
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)    
+    ax.set_xlabel('Number of archetypes', fontsize=20)
+    ax.set_ylabel('NMI', fontsize=20)
+
+    ax.set_ylim([0,1.05])
+    plt.legend(fontsize=20)
+
+    plt.show()
