c578bed2f1680a3c052944577958ffaf7b0d14ad
Wedenborg <emiliewedenborg@gmail.com>
2024-04-30 16:39:24 +0200

    Generating results

--- src/utils/additional_plotting_functions.py
+++ src/utils/additional_plotting_functions.py
@@ -18,7 +18,7 @@ matplotlib.rcParams['mathtext.fontset'] = 'stix'
 matplotlib.rcParams['font.family'] = 'STIXGeneral'
 
 # TODO: Make some nice error handling for K_list. Would be nice to have a default value for K_list
-def loss_archetype_plot(K_list, results_path: str = 'synthetic_results/1000_complex_results.json',results_path2: str = 'C:/Users/aejew/Downloads/AA_results/AA_results/naive_OSM_results/all_AA_results.json'):
+def loss_archetype_plot(K_list, results_path: str = 'synthetic_results/1000_complex_results.json',results_path2: str = None):
     """
     A plot over the final loss obtained as a function of the number of archetypes.
     Parameters:
@@ -30,17 +30,20 @@ def loss_archetype_plot(K_list, results_path: str = 'synthetic_results/1000_comp
         df_res = pd.DataFrame(result)
 
 
-    with open(f'{results_path2}', 'r') as f:
-        result2 = json.load(f)
-        df_res2 = pd.DataFrame(result2)
+    if results_path2:
+        with open(f'{results_path2}', 'r') as f:
+            result2 = json.load(f)
+            df_res2 = pd.DataFrame(result2)
 
-    df_res2.method = df_res2.method.replace({'CAA': 'TSAA'})    
+        df_res2.method = df_res2.method.replace({'CAA': 'TSAA'})    
+        df_res = pd.concat([df_res,df_res2])
+
     df_res.method = df_res.method.replace({'OAA': 'OAA', 'RBOAA': 'RBOAA', 'CAA': 'AA','TSAA': 'TSAA'})  
 
-    df_res = pd.concat([df_res,df_res2])  
     methods = df_res['method'].unique()
 
-    methods_colors = dict(zip(methods.tolist(), ["#EF476F", "#FFD166", "#06D6A0", "#073B4C"]))
+    methods_colors = {'RBOAA': "#EF476F", 'OAA': "#FFD166", 'AA': "#06D6A0","TSAA" : "#073B4C"}
+    #methods_colors = dict(zip(methods.tolist(), ["#EF476F", "#FFD166", "#06D6A0", "#073B4C"]))
     fig, ax = plt.subplots(figsize = (15,5), layout='constrained')
 
     def add_curve(analysis_archetypes, losses, is_min: bool, method: str):
@@ -50,17 +53,15 @@ def loss_archetype_plot(K_list, results_path: str = 'synthetic_results/1000_comp
             # plt.scatter(analysis_archetypes, losses)
             plt.plot(analysis_archetypes, losses, alpha=0.3, c=methods_colors[method])
 
-    ax.set_xlabel('Number of archetypes', fontsize=25)
-    ax.set_ylabel('Cross entropy loss', fontsize=25)
+    ax.set_xlabel('Number of archetypes', fontsize=30)
+    ax.set_ylabel('Cross entropy loss', fontsize=30)
     ax2 = ax.twinx()
-    ax2.set_ylabel('SSE',fontsize=25)
+    ax2.set_ylabel('SSE',fontsize=30)
 
     # TODO: Automatic set of ax2 ylim to avoid the two plots starting the same place something likemin = min(min_loss_AA, min_loss_TSOAA)*0.9 and max  = max(max_loss_AA, max_loss_TSOAA)*1.1
     #ax2.set_ylim((np.min(df_res[df_res.method=='AA'].loss[-1])-1000,np.max(df_res[df_res.method=='TSAA'].loss[0])+1000))
     #ax.set_ylim((np.min(df_res[df_res.method=='RBOAA'].loss[-1])-1000,np.max(df_res[df_res.method=='OAA'].loss[0])+1000))
     
-
-
     for method in methods:
         df_losses = df_res.loc[df_res['method'] == method][['n_archetypes', 'loss']]
         analysis_archetypes = K_list #df_losses['n_archetypes'].unique().tolist()
@@ -72,6 +73,9 @@ def loss_archetype_plot(K_list, results_path: str = 'synthetic_results/1000_comp
         tmp = losses[losses == np.min(losses, axis=1)[:, None]]
         _, idx = np.unique(losses[losses == np.min(losses, axis=1)[:, None]], return_index=True)
         min_losses = tmp[np.sort(idx)]
+
+        print(str(method)+str(idx))
+        print(tmp)
 
         
         if method in ['AA','TSAA']:
@@ -91,24 +95,24 @@ def loss_archetype_plot(K_list, results_path: str = 'synthetic_results/1000_comp
     
 
     # Not sure what diff between minor and major is
-    ax.tick_params(axis='both', which='major', labelsize=20)
-    ax.tick_params(axis='both', which='minor', labelsize=20)
+    ax.tick_params(axis='both', which='major', labelsize=25)
+    ax.tick_params(axis='both', which='minor', labelsize=25)
 
-    ax2.tick_params(axis='both', which='major', labelsize=20,colors = "#073B4C")
-    ax2.tick_params(axis='both', which='minor', labelsize=20)
+    ax2.tick_params(axis='both', which='major', labelsize=25,colors = "#073B4C")
+    ax2.tick_params(axis='both', which='minor', labelsize=25)
 
-    #ax2.set_ylim[2000,11000]
+    #ax2.set_ylim[2500,11000]
 
     ax2.spines['right'].set_color(methods_colors["AA"])
     
     lines, labels = ax.get_legend_handles_labels()
     lines2, labels2 = ax2.get_legend_handles_labels()
-    ax2.legend(lines + lines2, labels + labels2, loc=0,fontsize=25)
+    ax2.legend(lines + lines2, labels + labels2, loc=0,fontsize=30)
 
 
-# loss_archetype_plot(df_res_20)
+# loss_archetype_plot(df_res_25)
 
-def NMI_archetypes(K_list, results_path: str = 'synthetic_results/1000_complex_results.json',results_path2: str = 'C:/Users/aejew/Downloads/AA_results/AA_results/naive_OSM_results/all_AA_results.json'):
+def NMI_archetypes(K_list, results_path: str = 'synthetic_results/1000_complex_results.json',results_path2: str = None):
     """
     A plot over the final loss obtained as a function of the number of archetypes
     """
@@ -116,31 +120,33 @@ def NMI_archetypes(K_list, results_path: str = 'synthetic_results/1000_complex_r
         result = json.load(f)
         df_res = pd.DataFrame(result)
 
-    
-    with open(f'{results_path2}', 'r') as f:
-        result2 = json.load(f)
-        df_res2 = pd.DataFrame(result2)
+    if results_path2:
+        with open(f'{results_path2}', 'r') as f:
+            result2 = json.load(f)
+            df_res2 = pd.DataFrame(result2)
 
-    df_res2.method = df_res2.method.replace({'CAA': 'TSAA'})    
-    df_res.method = df_res.method.replace({'OAA': 'OAA', 'RBOAA': 'RBOAA', 'CAA': 'AA','TSAA': 'TSAA'})  
+        df_res2.method = df_res2.method.replace({'CAA': 'TSAA'})    
+        df_res = pd.concat([df_res,df_res2])
+    df_res.method = df_res.method.replace({'RBOAA': 'RBOAA', 'OAA': 'OAA', 'CAA': 'AA','TSAA': 'TSAA'})  
 
-    df_res = pd.concat([df_res,df_res2])      
+          
     df_res = df_res[df_res['n_archetypes'].isin(K_list)]
 
     methods = df_res['method'].unique()
-    methods_colors = dict(zip(methods.tolist(), ["#EF476F", "#FFD166", "#06D6A0", "#073B4C"]))
+    #methods_colors = dict(zip(methods.tolist(), ["#EF476F", "#FFD166", "#06D6A0", "#073B4C"]))
+    methods_colors = {'RBOAA': "#EF476F", 'OAA': "#FFD166", 'AA': "#06D6A0","TSAA" : "#073B4C"}
     fig, ax = plt.subplots(1,1,figsize = (15,5), layout='constrained')
 
     ax = sns.boxplot(x='n_archetypes', y="NMI", hue="method", showmeans=True, data=df_res,palette=methods_colors,meanprops={"marker": "s", "markerfacecolor": "white", "markeredgecolor": "black"})
     ax.xaxis.grid(True, which='major')
     [ax.axvline(x+.5,color='k') for x in ax.get_xticks()]
-    plt.xticks(fontsize=20)
-    plt.yticks(fontsize=20)    
-    ax.set_xlabel('Number of archetypes', fontsize=25)
-    ax.set_ylabel('NMI', fontsize=25)
+    plt.xticks(fontsize=25)
+    plt.yticks(fontsize=25)    
+    ax.set_xlabel('Number of archetypes', fontsize=30)
+    ax.set_ylabel('NMI', fontsize=30)
 
     ax.set_ylim([0,1.05])
-    plt.legend(fontsize=25,loc="upper right")
+    plt.legend(fontsize=30,loc="upper right")
 
 
 
@@ -194,19 +200,20 @@ def plot_NMI_stability(folder_path, K_list, repetitions= 10):
 
         
     methods = df['Method'].unique()
-    methods_colors = dict(zip(methods.tolist(), ["#EF476F", "#FFD166", "#06D6A0", "#073B4C"]))
+    #methods_colors = dict(zip(methods.tolist(), ["#EF476F", "#FFD166", "#06D6A0", "#073B4C"]))
+    methods_colors = {'RBOAA': "#EF476F", 'OAA': "#FFD166", 'AA': "#06D6A0","TSAA" : "#073B4C"}
     fig, ax = plt.subplots(1,1,figsize = (15,5), layout='constrained')
 
     ax = sns.boxplot(x='Archetypes', y="NMI", hue="Method", showmeans=True, data=df,palette=methods_colors,meanprops={"marker": "s", "markerfacecolor": "white", "markeredgecolor": "black"})
     ax.xaxis.grid(True, which='major')
     [ax.axvline(x+.5,color='k') for x in ax.get_xticks()]
-    plt.xticks(fontsize=20)
-    plt.yticks(fontsize=20)    
-    ax.set_xlabel('Number of archetypes', fontsize=25)
-    ax.set_ylabel('NMI', fontsize=25)
+    plt.xticks(fontsize=25)
+    plt.yticks(fontsize=25)    
+    ax.set_xlabel('Number of archetypes', fontsize=30)
+    ax.set_ylabel('NMI', fontsize=30)
 
     ax.set_ylim([0,1.05])
-    plt.legend(fontsize=25)
+    plt.legend(fontsize=30)
     plt.show()
 
 
